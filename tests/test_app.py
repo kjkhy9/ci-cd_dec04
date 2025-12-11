8b50b50b52daa7630683695944f228cfdf0be6b6
kjkhy9 <124001565+kjkhy9@users.noreply.github.com>
2025-12-10 19:01:55 -0600

    Rename tests from add to mult and update assertions

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,10 +4,10 @@ from pathlib import Path
 root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
 
-from app import add
+from app import mult
 
-def test_add():
+def test_mult():
     assert mult(5,6) == 30
 
-def test_add2():
+def test_mult2():
     assert mult(5,6) != 29
